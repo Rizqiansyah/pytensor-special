52dcc5c3e31ddd4a4f7c7d551cd225d37fb46b72
default <akbarrizqiansyah@yahoo.com>
2023-09-21 09:30:55 +1000

    gammaincinv

--- pytensor_special/math.py
+++ pytensor_special/math.py
@@ -1,5 +1,28 @@
 from .elemwise import scalar_elemwise
+import pytensor
+import numpy
 
-from .scalar import (
-    ndtr
-)
+def ndtr(mu, sigma, x):
+    """
+        Pytensor implementation of the normal distribution CDF
+
+        Parameters
+        ----------
+        mu: float
+            Mean of the normal distribution
+        sigma: float
+            Standard deviation of the normal distribution
+
+        Returns
+        -------
+        float
+            The CDF of the normal distribution at x
+    """
+    return 0.5*(1 + pytensor.tensor.erf((x-mu)/(sigma*numpy.sqrt(2))))
+
+@scalar_elemwise
+def gammaincinv(a, z):
+    """
+        Pytensor implementation of the inverse incomplete gamma function
+    """
+
--- pytensor_special/scalar.py
+++ pytensor_special/scalar.py
@@ -1,22 +1,46 @@
 import pytensor
 import scipy
-from pytensor.scalar.basic import UnaryScalarOp
-import numpy as np
+from pytensor.scalar.basic import BinaryScalarOp, ScalarOp
+from pytensor.scalar.basic import (
+    upgrade_to_float_no_complex, float_types
+)
 
-def ndtr(mu, sigma, x):
+class GammaIncInv(BinaryScalarOp):
     """
-        Pytensor implementation of the normal distribution CDF
-
-        Parameters
-        ----------
-        mu: float
-            Mean of the normal distribution
-        sigma: float
-            Standard deviation of the normal distribution
-
-        Returns
-        -------
-        float
-            The CDF of the normal distribution at x
+    Inverse of regularized lower incomplete gamma function
     """
-    return 0.5*(1 + pytensor.tensor.erf((x-mu)/(sigma*np.sqrt(2))))
+    nin = 2
+    nfunc_spec = ('scipy.special.gammaincinv', 2, 1)
+
+    def impl(self, a, z):
+        return scipy.special.gammaincinv(a, z)
+    
+    def grad(self, inp, grads):
+        #raise NotImplementedError()
+        a, z = inp
+        (gz, ) = grads
+        
+        x = GammaIncInv(upgrade_to_float_no_complex, name='gammaincinv_wrtx_nograd')(a, z)
+
+        dx_dz = pytensor.scalar.math.gamma(a) / pytensor.scalar.basic.pow(x, a-1) * pytensor.scalar.basic.exp(-x)
+        dz_da = pytensor.scalar.math.gammainc_grad(a, z)
+        dx_da = dx_dz * dz_da
+
+        return [
+            gz * dx_da,
+            gz * dx_dz
+        ]
+    
+    def c_code(self, node, name, inputs, outputs, sub):
+        raise NotImplementedError()
+        # (a, z,) = inputs
+        # (x,) = outputs
+        # if node.inputs[0].type in float_types:
+        #     return """
+                
+
+        #     """ % (locals())
+        # raise NotImplementedError("only floating point is implemented")
+    
+gammaincinv = GammaIncInv(upgrade_to_float_no_complex, name='gammaincinv')
+
